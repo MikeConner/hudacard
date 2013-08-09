e168f90f016f1d54bfa9b23dd33c3fc01baa4454
arash <arash.danaie@gmail.com>
2013-08-08 22:14:42 -0400

    change to bitcoin gateway - production

--- config/initializers/blockchain.rb
+++ config/initializers/blockchain.rb
@@ -4,6 +4,6 @@ require 'bitcoin_test_gateway'
 MERCHANT_KEY = '61e9e217-d259-4ed2-9939-09ce5f071fe4'
 MERCHANT_PASSWORD = ENV['MERCHANT_PASSWORD'] || '$tamP3d3$!@'
 
-BITCOIN_GATEWAY = BitcoinTestGateway.instance
+BITCOIN_GATEWAY = BitcoinGateway.instance
 
 ADMIN_KEY = 'bd0d9205-7680-4d15-8f37-985d84871569'
